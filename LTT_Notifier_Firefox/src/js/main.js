ef0bcaab0a6b4daf5fae5e4a54a5366588b66ac4
AluminiumTech <aluminiumdev@gmail.com>
2019-02-04 23:03:27 +0000

    fix the copyright notices since we switched to Apache 2.0 a while ago

--- LTT_Notifier_Firefox/src/js/main.js
+++ LTT_Notifier_Firefox/src/js/main.js
@@ -3,6 +3,20 @@ Part of AluminiumTech's Unofficial LTT Notifier for Firefox Extension.
 
 Created by AluminiumTech.
 Version: 0.1
+
+Copyright 2018-2019 AluminiumTech
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 const baseUrl = "https://linustechtips.com/main/";
@@ -59,7 +73,8 @@ var messageCount = 0,
   etag = {
   			ltt: "",
   			tq: "",
-  			csf: ""
+  			csf: "",
+        tl:"",
   		}, //holds the etag from the youtube update to reduce download usage
   messagesShown = JSON.parse(settings.messagesShown),
   panel,
@@ -94,5 +109,5 @@ function createNotification(data, notificationShowing){
 }
 
 function requestNotificationPermission(){
- notifications.requestPermission(); 
+ notifications.requestPermission();
 }
